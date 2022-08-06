bd1dabbaacbfec435ffeef2f9adc6dcb50c8a80f
Martha Quintanilla-Ramirez <marthaquin21@gmail.com>
2022-08-06 09:41:01 -0700

    added process.env.JWTEXPIRES

--- src/models/users/models.js
+++ src/models/users/models.js
@@ -21,7 +21,7 @@ const userModel = (sequelize, DataTypes) => {
             type: DataTypes.VIRTUAL, //this means this isnt going to get stored in the data base, only avail. at runtime
             get() {
                 const payload = { username: this.username, role: this.role };
-                return jwt.sign(payload, SECRET, { expiresIn: JWTEXPIRES });// JWT expires in 1 min. tested in Insomnia/Postman/Thunderclient and it works. entered into .env
+                return jwt.sign(payload, SECRET, { expiresIn: process.env.JWTEXPIRES });// JWT expires in 1 min. tested in Insomnia/Postman/Thunderclient and it works. entered into .env
             },
         },
     });
